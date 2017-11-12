428e51cc6bc9f898e57976ba22d9debf29eb3eda
Yash Sharma <yash4688@gmail.com>
2017-11-12 15:45:41 +0530

    Add shortening algorithm + Add database connections

---
+++ models/url.js
@@ -0,0 +1,41 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var CounterSchema = Schema({
+    _id: {
+        type: String,
+        required: true
+    },
+    seq: {
+        type: Number,
+        default: 0
+    }
+});
+
+var counter = mongoose.model('counter', CounterSchema);
+
+var urlSchema = Schema({
+    _id: {
+        type: Number,
+        index: true
+    },
+    longUrl: String,
+    createdAt: Date
+});
+
+urlSchema.pre('save', (next) => {
+    var doc = this;
+
+    counter.findByIdAndUpdate({_id: 'url_count'}, {$inc: {seq: 1 }}, (error, counter) => {
+        if(error){
+            return next(error);
+        }
+        doc._id = counter.seq;
+        doc.createdAt = new Date();
+        next();
+    });
+});
+
+var Url = mongoose.model('url', urlSchema);
+
+module.exports = Url;
